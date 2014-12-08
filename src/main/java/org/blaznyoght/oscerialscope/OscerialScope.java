429ad1213a5d893758d16d2630af59e1e5874ae4
Pierre-Yves Aillet <blaznyoght@gmail.com>
2014-12-08 07:07:48 +0100

    Internationalization

--- src/main/java/org/blaznyoght/oscerialscope/OscerialScope.java
+++ src/main/java/org/blaznyoght/oscerialscope/OscerialScope.java
@@ -1,7 +1,12 @@
 package org.blaznyoght.oscerialscope;
 
+import java.util.Locale;
+
+import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
 import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
 import org.apache.pivot.wtk.Window;
 
@@ -9,15 +14,22 @@ import org.apache.pivot.wtk.Window;
 public class OscerialScope implements Application {
     private Window window = null;
 
+    public static final String LANGUAGE_KEY = "language";
+
 	public static void main(String[] args) {
-		new OscerialScope();
+		DesktopApplicationContext.main(OscerialScope.class, args);
 	}
 
 	@Override
 	public void startup(Display display, Map<String, String> properties)
 			throws Exception {
-		// TODO Auto-generated method stub
-		
+        String language = properties.get(LANGUAGE_KEY);
+        Locale locale = (language == null) ? Locale.getDefault() : new Locale(language);
+        Resources resources = new Resources(OscerialScope.class.getName(), locale);
+        
+		BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window)bxmlSerializer.readObject(getClass().getResource("OscerialScope.bxml"), resources);
+        window.open(display);
 	}
 
 	@Override
